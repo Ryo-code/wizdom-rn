326cc0a86eb99250fd51b586f1a48f088adcfad1
mikeislearning <tothemac@gmail.com>
2017-06-06 20:50:55 -0400

    restructured content.js into separate functions

--- src/components/content.js
+++ src/components/content.js
@@ -5,100 +5,94 @@ import { View, Text, ScrollView, Dimensions, Platform } from 'react-native';
 export default class Content extends Component {
   constructor(props) {
     super(props);
+    this.renderActive = this.renderActive.bind(this);
+    this.renderDefinition = this.renderDefinition.bind(this);
+    this.renderFact = this.renderFact.bind(this);
+    this.renderQuotation = this.renderQuotation.bind(this);
   }
 
-  render() {
-
-    const { wrapper, WOTD, defContainer, defTypeAndPronunciation, defBox, defType, 
+  renderDefinition(){
+    const { WOTD, defContainer, defTypeAndPronunciation, defBox, defType, 
       definitions, exampleText, exampleBox, didYouKnowBox, didYouKnow, 
       factContainer, factText, quoteContainer, quotation, quoter,
     } = styles;
-
-    let showDefinition = () => {
-      return (
-        <View style={defContainer}>
-          <View style={defBox}>
-            <Text style={WOTD}>whatever </Text>
-            <Text style={defTypeAndPronunciation}>
-              <Text> what·ev·er | </Text>
-              <Text style={defType}>pronoun</Text>
-            </Text>
-          </View>
-          <Text style={definitions}>
-            <Text>a : anything or everything that take whatever you want </Text>
-            <Text> | b : no matter what whatever he says, they won't believe him</Text>
-            <Text> | c : whatnot enjoys skiing, hiking, or whatever</Text>
+    return (
+      <View style={defContainer}>
+        <View style={defBox}>
+          <Text style={WOTD}>whatever </Text>
+          <Text style={defTypeAndPronunciation}>
+            <Text> what·ev·er | </Text>
+            <Text style={defType}>pronoun</Text>
           </Text>
-          <Text style={exampleBox}>
-            <Text style={exampleText}>Example 1: </Text>
-            <Text> I asked how his day was and all he said was "whatever..." it's so annoying!</Text>
-          </Text>
-          <Text style={exampleBox}>
-            <Text style={exampleText}>Example 2: </Text>
-            <Text> So Tiffany said to Joadie that Beth was all like "WHATEVER" about Greg, but Lawny said that Greg's friend John said he said something totally different, like omg!</Text>
-          </Text>
-          <ScrollView contentContainerStyle={didYouKnowBox}>
-            <Text style={didYouKnow}>Food for Thought - a poem about Whatever: </Text>
-            <Text>Whatever is we only know As in our minds we find it so; No staring fact is half so clear As one dim, preconceived idea -- No matter how the fact may glow. Vainly may Truth her trumpet blow To stir our minds; like heavy dough They stick to what they think — won’t hear Whatever is. Our ancient myths in solid row Stand up — we simply have to go And choke each fiction old and dear Before the modest facts appear; Then we may grasp, reluctant, slow, Whatever is.</Text>
-            <Text> </Text>
-          </ScrollView>
         </View>
-      );
-    };
+        <Text style={definitions}>
+          <Text>a : anything or everything that take whatever you want </Text>
+          <Text> | b : no matter what whatever he says, they won't believe him</Text>
+          <Text> | c : whatnot enjoys skiing, hiking, or whatever</Text>
+        </Text>
+        <Text style={exampleBox}>
+          <Text style={exampleText}>Example 1: </Text>
+          <Text> I asked how his day was and all he said was "whatever..." it's so annoying!</Text>
+        </Text>
+        <Text style={exampleBox}>
+          <Text style={exampleText}>Example 2: </Text>
+          <Text> So Tiffany said to Joadie that Beth was all like "WHATEVER" about Greg, but Lawny said that Greg's friend John said he said something totally different, like omg!</Text>
+        </Text>
+        <ScrollView contentContainerStyle={didYouKnowBox}>
+          <Text style={didYouKnow}>Food for Thought - a poem about Whatever: </Text>
+          <Text>Whatever is we only know As in our minds we find it so; No staring fact is half so clear As one dim, preconceived idea -- No matter how the fact may glow. Vainly may Truth her trumpet blow To stir our minds; like heavy dough They stick to what they think — won’t hear Whatever is. Our ancient myths in solid row Stand up — we simply have to go And choke each fiction old and dear Before the modest facts appear; Then we may grasp, reluctant, slow, Whatever is.</Text>
+          <Text> </Text>
+        </ScrollView>
+      </View>
+    );
+  }
 
-    let showFact = () => {
+  renderFact() {
+    const { factContainer, factText } = styles;
       return (
         <View style={factContainer}>
           <Text style={factText}> Penguins swim up to 3,100 miles in a year </Text>
         </View>
       );
-    };
+  }
 
-    let showQuotation = () => {
+  renderQuotation() {
+    const { WOTD, defContainer, defTypeAndPronunciation, defBox, defType, 
+      definitions, exampleText, exampleBox, didYouKnowBox, didYouKnow, 
+      factContainer, factText, quoteContainer, quotation, quoter,
+    } = styles;
       return (
         <View style={quoteContainer}>
           <Text style={quotation}>We only have to look around us to see how complexity and psychic temperature are still rising: and rising no longer on the scale of the individual but now on that of the planet. This indication is so familiar to us that we cannot but recognize the objective, experiential, reality of a transformation of the planet as a whole.</Text>
           <Text style={quoter}> ~ Pierre Teilhard de Chardin </Text>
         </View>
       );
-    };
+  }
 
-    const tabs = [
-      {
-        title: 'definition',
-        content: showDefinition(),
-      }, {
-        title: 'fact',
-        content: showFact(),
-      }, {
-        title: 'quotation',
-        content: showQuotation(),
-      }
-    ];
+  renderActive(){
+    switch (this.props.current) {
+      case 'definition':
+        console.log('boop')
+        return this.renderDefinition();
+        break;
+      case 'fact':
+        return this.renderFact();
+        break;
+      case 'quotiation':
+        return renderQuotation();
+        break;
+      default:
+        return this.renderDefinition();
+    }
+  }
 
-    const renderContentIfActive = (tab) => {
-      {console.log("    tab~~~~~~~~~!!!", tab.title)}
-      {console.log("current=========!!!", this.props.current)}
-        let currentContent = tab.content;
-      if(this.props.current === tab.title) {
-        console.log("It's a match!");
-      }
-        return currentContent;
-    };
+  render() {
+    const { wrapper } = styles;
+    const Active = this.renderActive;
 
     return (
       <ScrollView contentContainerStyle={wrapper}>
-        {
-          renderContentIfActive(tabs[0])
-
-          // for (var i = 0; i < tabs.length; i++){
-          //   renderContentIfActive(tab[i]);
-          // }
-          
-          // tabs.forEach((tab) => {
-          //   renderContentIfActive(tab);
-          // })
-        }
+        <Active />
       </ScrollView>
     );
   }
